2f7b306580c64a542172b537b4c676a65c6e6780
Thiago Correia <dasilvacorreiathiago@gmail.com>
2021-06-09 14:31:33 -0300

    added bot

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import pyautogui, time
 
 time.sleep(5) # Hold on a few seconds to execute the program 
 
-f = open("backstreetboys", "r") # Open the file
+f = open("backstreetboys", "r") # Open the file and read
 
 # Create a loop for any word in the file on f 
 for word in f:
